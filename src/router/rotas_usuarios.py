1df002409864953cd1fe72a60130576fc2095a96
Marcos Sousa <a.marcos.car@bol.com.br>
2022-05-11 17:58:09 -0300

    Alembic: Migrations implementado

--- src/router/rotas_usuarios.py
+++ src/router/rotas_usuarios.py
@@ -11,20 +11,24 @@ def create_user(user: Usuario, session: Session = Depends(get_db)):
     created_user = RepositorioUsuarios(session).create_user(user)
     return created_user
 
-@router.delete('/usuario')
-def delete_user():
-    pass
-
-@router.put('/usuario')
-def update_user():
-    pass
 
 @router.get('/usuario', status_code=status.HTTP_202_ACCEPTED, response_model=List[UsuarioSimples])
 def read_user_list(session: Session = Depends(get_db)):
     list_user = RepositorioUsuarios(session).read_user()
     return list_user
 
-@router.get('usuario/{id}')
-def read_user_id():
+
+@router.delete('/usuario')
+def delete_user():
+    pass
+
+
+@router.put('/usuario')
+def update_user():
+    pass
+
+
+@router.get('/usuario/{id}')
+def read_user_id(id: int):
     pass
 
