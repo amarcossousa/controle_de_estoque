750c1dfa48690e9c0e25bb85236ff0e092bdcf6b
Marcos Sousa <a.marcos.car@bol.com.br>
2022-05-10 23:19:37 -0300

    Schemas: Primeira versão dos eschemas

--- src/schema/schemas.py
+++ src/schema/schemas.py
@@ -1,15 +1,35 @@
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional
 
-class Usuarios:
-    pass
-
-class Categoria:
-    pass
-
-class Produto:
-    pass
-
-class Vendas:
-    pass
+class Categorias(BaseModel):
+    id : Optional[int] = None
+    nome_categoria : str
 
 
+class Produto(BaseModel):
+    id : Optional[int] = None
+    nome : str
+    quantidade : int
+    descricao : str
+    tamanho : int
+    preco : float
+    image : bytes
+    ativo : bool
+    # categoria : Forenkey de categoria 
 
+
+class Usuario(BaseModel):
+    id : Optional[int] = None
+    telefone : str
+    cpf : str
+    senha : str
+
+
+class Vendas(BaseModel):
+    id : Optional[int] = None
+    total : int
+    quantidade : int
+    preco_unitario : float
+    data : Optional[datetime] = None
+    
